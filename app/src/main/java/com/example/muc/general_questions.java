c9b9872cbb2ad36f438924d0514947a187d59a9f
leighanrak <leighannraksasouk@gmail.com>
2019-10-21 19:05:19 -0400

    created new branch & new screen for general questions

---
+++ app/src/main/java/com/example/muc/general_questions.java
@@ -0,0 +1,14 @@
+package com.example.muc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class general_questions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_questions);
+    }
+}
